797777c6b591203bc0c77c47f0ae48fd44e7cca1
HongLabInc <honglab2021@gmail.com>
2025-08-28 17:55:26 -0700

    Update

--- examples/Ex12_Gltf/Ex12_Gltf.cpp
+++ examples/Ex12_Gltf/Ex12_Gltf.cpp
@@ -4,7 +4,14 @@ using namespace hlab;
 
 int main()
 {
-    Application().run();
+    /* Currently, which models to load from beginning is hardcoded in Application class.
+     * Is there a way to specify which model (or models) to read here?
+     */
+
+    // 안내: 힙 메모리 사용하기 위해 unique_ptr 사용
+    auto app = std::make_unique<Application>();
+
+    app->run();
 
     return 0;
 }
