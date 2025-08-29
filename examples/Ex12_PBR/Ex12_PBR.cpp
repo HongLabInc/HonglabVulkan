b5de77249f261ae8a74562bc4ecb3b27e19b1d8f
HongLabInc <honglab2021@gmail.com>
2025-08-29 14:11:46 -0700

    Helmet example setting.

--- examples/Ex12_PBR/Ex12_PBR.cpp
+++ examples/Ex12_PBR/Ex12_PBR.cpp
@@ -6,9 +6,9 @@ int main()
 {
     ApplicationConfig config;
 
-    config.models.push_back(
-        ModelConfig("models/DamagedHelmet.glb", "Helmet")
-            .setTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f))));
+    config.models.push_back(ModelConfig("models/DamagedHelmet.glb", "Helmet")
+                                .setTransform(glm::rotate(glm::mat4(1.0f), glm::radians(90.0f),
+                                                          glm::vec3(1.0f, 0.0f, 0.0f))));
 
     config.camera = config.camera.forHelmet();
 
