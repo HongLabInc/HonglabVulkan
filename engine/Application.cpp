43e1e376969fecb1c708d56538eeab8b299438f8
HongLabInc <honglab2021@gmail.com>
2026-04-07 13:09:28 -0700

    fix: PBR 셰이딩 및 카메라 위치 수정 (deferred path 포함)
    - 월드 카메라 위치를 inverse(view)[3]로 추출하도록 변경
      (기존: camera_.position을 그대로 사용해 회전이 반영되지 않음)
    - IBL specular reflect()에 incident vector를 넘기도록 -v 적용
      - deferredLighting.comp (이 브랜치에서 실제 사용되는 셰이더)
      - pbrForward.frag (미사용이지만 일관성 유지)
    - Metallic / Roughness 슬라이더 라벨 swap 수정

--- engine/Application.cpp
+++ engine/Application.cpp
@@ -404,7 +404,8 @@ void Application::run()
             camera_.update(deltaTime);
             renderer_->sceneUBO().projection = camera_.matrices.perspective;
             renderer_->sceneUBO().view = camera_.matrices.view;
-            renderer_->sceneUBO().cameraPos = camera_.position;
+            renderer_->sceneUBO().cameraPos =
+                glm::vec3(glm::inverse(camera_.matrices.view)[3]);
         }
 
         {
@@ -902,8 +903,8 @@ void Application::updateGui()
         ImGui::SliderFloat(format("DiffuseWeight##{}", i).c_str(), &(m.coeffs()[1]), 0.0f, 10.0f);
         ImGui::SliderFloat(format("EmissiveWeight##{}", i).c_str(), &(m.coeffs()[2]), 0.0f, 10.0f);
         ImGui::SliderFloat(format("ShadowOffset##{}", i).c_str(), &(m.coeffs()[3]), 0.0f, 1.0f);
-        ImGui::SliderFloat(format("RoughnessWeight##{}", i).c_str(), &(m.coeffs()[4]), 0.0f, 1.0f);
-        ImGui::SliderFloat(format("MetallicWeight##{}", i).c_str(), &(m.coeffs()[5]), 0.0f, 1.0f);
+        ImGui::SliderFloat(format("MetallicWeight##{}", i).c_str(), &(m.coeffs()[4]), 0.0f, 1.0f);
+        ImGui::SliderFloat(format("RoughnessWeight##{}", i).c_str(), &(m.coeffs()[5]), 0.0f, 1.0f);
 
         // Extract and edit position
         glm::vec3 position = glm::vec3(m.modelMatrix()[3]);
